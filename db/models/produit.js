5442b1031ee67db0e0ceb5be300df368482ef63f
Guidy Ange Isaac <packer02andro69@gmail.com>
2024-11-25 10:08:39 +0000

    premier commit

---
+++ db/models/produit.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+
+const schemaProduit = new Schema({
+    nom: {
+        type: String,
+        required: true,
+    },
+    prix: {
+        type: Number,
+        required: true,
+    },
+    refference: {
+        type: String,
+        required: true,
+    },
+    poids: {
+        type: Number,
+        required: true,
+    },
+    date : {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export const Produit = model("Produit", schemaProduit);
